776a5aa3d4628b1d224a45f060395d6d38938522
Ant <antsplat@gmail.com>
2023-11-01 21:28:47 -0400

    Create default cow behavior and help message

--- cowsay.py
+++ cowsay.py
@@ -2,6 +2,11 @@ import sys
 from heifer_generator import HeiferGenerator
 
 
+# Throw an error if a cow wasn't found
+class CowNotFoundError(Exception):
+    pass
+
+
 # List all cows. Accepts list of cow objects.
 def list_cows(cows):
     for cow in cows:
@@ -10,47 +15,78 @@ def list_cows(cows):
 
 # Find a specific cow. Accepts a string name and a list of cow objects
 def find_cow(name, cows):
-
     # For each cow in the list
     for cow in cows:
-        # If the name matches then return the cow
-        print(f"name is {name}")
-        print(f"cow.get_name is{cow.get_name()}")
-
+        # If the name is a cow name
         if name == cow.get_name():
-            cow_index = HeiferGenerator.cowNames.index(name)
-            cow.set_image(HeiferGenerator.cowImages[cow_index])
+            # Set the image of the cow using the index of the name of the cow
+            cow.set_image(HeiferGenerator.cowImages[HeiferGenerator.cowNames.index(name)])
+            # Return the cow
             return cow
 
     # Return None if no match was found
     return None
 
 
-
-if __name__ == "__main__":
+def main():
+    # Get a list of cows
     cows = HeiferGenerator.get_cows()
 
     # TODO test for bad input
-    # try:
+    try:
 
-    # Print the list of available cows
-    # Usage: cowsay.py -l
-    if sys.argv[1] == '-l':
-        list_cows(cows)
+        # Print a help message
+        if sys.argv[1] == '--help':
+            print("usage: cowsay.py [OPTION] MESSAGE")
+            print("-l")
+            print("\tList available cows")
+            print()
+            print("-n [COW] [MESSAGE]")
+            print("\tSpecify a cow and a message")
+            print()
+            print("If no flag is specified, the message will be printed with the default cow.")
 
-        # Print a message using a specified cow
-        # Usage: cowsay.py -n COW MESSAGE
-    elif sys.argv[1] == '-n':
+        # Print the list of available cows
+        # Usage: cowsay.py -l
+        elif sys.argv[1] == '-l':
+            list_cows(cows)
 
-        # Get the specified cow
-        # TODO throw an error if the cow isn't found
-        # TODO Why is this sys.argv[2], not sys.argv[3]?
-        cow = find_cow(sys.argv[2], cows)
-        print("sys.argv[2] is", sys.argv[2])
-        print("cow is", cow)
-        print(cows)
-        print(sys.argv[3:])
-        print(cow.get_image())
+            # Print a message using a specified cow
+            # Usage: cowsay.py -n COW MESSAGE
+        elif sys.argv[1] == '-n':
+            try:
+                # Get the inputted cow image and print it
+                cow = find_cow(sys.argv[2], cows)
 
-    # Print a message with the default cow
-    # Usage: cowsay.py MESSAGE
+                # Throw an error if a cow wasn't found
+                if cow is None:
+                    raise CowNotFoundError
+
+                # Print the message and speech bubble lines
+                print(sys.argv[3])
+                print(HeiferGenerator.quoteLines)
+                print(cow.get_image())
+
+            # Print that the requested cow wasn't found
+            except CowNotFoundError:
+                print(f"Could not find {sys.argv[2]} cow!")
+
+        # Print a message with the default cow
+        # Usage: cowsay.py MESSAGE
+        # No flag was specified
+        else:
+            # Print the message and speech bubble lines
+            print(sys.argv[1])
+            print(HeiferGenerator.quoteLines)
+
+            # Print the default cow, which is the first entry in the heifer generator
+            cows[0].set_image(HeiferGenerator.cowImages[0])
+            print(cows[0].get_image())
+
+    except IndexError:
+        raise SystemExit("usage: cowsay.py [OPTION]\nTry 'cowsay.py --help' for more information.")
+
+
+# Call main
+if __name__ == "__main__":
+    main()
